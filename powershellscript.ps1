fcb7bb40c1a4cafd0ce54ef0b3efaacf57a01955
snipershane0305 <snipershane0305@gmail.com>
2024-08-08 14:11:48 -0400

    Update powershellscript.ps1

--- powershellscript.ps1
+++ powershellscript.ps1
@@ -39,25 +39,25 @@ bcdedit /set linearaddress57 OptOut
 bcdedit /set nx OptIn                  #enables data execution prevention which improves security
 
 write-host "applying network settings" -ForegroundColor red
-netsh int tcp set global rss = enabled
-netsh int tcp set global prr= enable
-netsh int tcp set global initialRto=2000
-netsh int tcp set global ecncapability= enable
-netsh int tcp set global rsc= disable
-netsh int tcp set supplemental Template=Internet CongestionProvider=bbr2
-netsh int tcp set supplemental Template=Datacenter CongestionProvider=bbr2
-netsh int tcp set supplemental Template=Compat CongestionProvider=bbr2
-netsh int tcp set supplemental Template=DatacenterCustom CongestionProvider=bbr2
-netsh int tcp set supplemental Template=InternetCustom CongestionProvider=bbr2
+netsh int tcp set global rss = enabled         #enables recieve side scaling which lets more than one cpu core handle tcp
+netsh int tcp set global prr= enable           #helps a tcp connection from recovering from packet loss quicker for less latency
+netsh int tcp set global initialRto=2000       #lowers initial retransmition timout which helps latency
+netsh int tcp set global ecncapability= enable #ecncapability will only be used if both the client and server support it
+netsh int tcp set global rsc= disable          #disables receive segment coalescing which makes small packets combine, this helps with computing many packets but at the cost of latency
+netsh int tcp set supplemental Template=Internet CongestionProvider=bbr2         #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
+netsh int tcp set supplemental Template=Datacenter CongestionProvider=bbr2       #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
+netsh int tcp set supplemental Template=Compat CongestionProvider=bbr2           #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
+netsh int tcp set supplemental Template=DatacenterCustom CongestionProvider=bbr2 #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
+netsh int tcp set supplemental Template=InternetCustom CongestionProvider=bbr2   #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
 netsh int ipv4 set dynamicport tcp start=1025 num=64511
 netsh int ipv4 set dynamicport udp start=1025 num=64511
-netsh int teredo set state disabled
-Set-NetOffloadGlobalSetting -PacketCoalescingFilter Disabled
-Set-NetOffloadGlobalSetting -ReceiveSegmentCoalescing Disabled
-Set-NetOffloadGlobalSetting -Chimney Disabled
-Enable-NetAdapterChecksumOffload -Name *
-Set-NetTCPSetting -SettingName InternetCustom -InitialCongestionWindow 10
-Disable-NetAdapterLso -Name *
+netsh int teredo set state disabled #disables teredo
+Set-NetOffloadGlobalSetting -PacketCoalescingFilter Disabled   #disables more coalescing
+Set-NetOffloadGlobalSetting -ReceiveSegmentCoalescing Disabled #disables more coalescing
+Set-NetOffloadGlobalSetting -Chimney Disabled #forces cpu to handle network instead of NIC
+Enable-NetAdapterChecksumOffload -Name *      #forces cpu to handle network instead of NIC
+Set-NetTCPSetting -SettingName InternetCustom -InitialCongestionWindow 10 #raises the initial congestion window which makes a tcp connection start with more bandwidth
+Disable-NetAdapterLso -Name * #disables large send offload which uses NIC instead of cpu (using the cpu for handing network tasks can help latency if your cpu is strong enough)
 
 write-host "setting dns" -ForegroundColor red
 Set-DnsClientServerAddress -interfaceindex 1 -serveraddresses ("9.9.9.9","1.1.1.1")
