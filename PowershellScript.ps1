bc62a946b8ac24225b5fc6f7ae606a7d8cf86633
snipershane0305 <snipershane0305@gmail.com>
2024-10-28 23:05:41 -0400

    added defender exclusions

--- PowershellScript.ps1
+++ PowershellScript.ps1
@@ -74,6 +74,45 @@ Set-DnsClientServerAddress -interfaceindex 8 -serveraddresses ("9.9.9.11","9.9.9
 Set-DnsClientServerAddress -interfaceindex 9 -serveraddresses ("9.9.9.11","9.9.9.9")
 Set-DnsClientServerAddress -interfaceindex 10 -serveraddresses ("9.9.9.11","9.9.9.9")
 
+#excludes some safe default paths to reduce defender scan time
+Add-MpPreference -ExclusionPath $env:LOCALAPPDATA"\Temp\NVIDIA Corporation\NV_Cache"
+Add-MpPreference -ExclusionPath $env:PROGRAMDATA"\NVIDIA Corporation\NV_Cache"
+Add-MpPreference -ExclusionPath $env:LOCALAPPDATA"\AMD\DX9Cache"
+Add-MpPreference -ExclusionPath $env:LOCALAPPDATA"\AMD\DxCache"
+Add-MpPreference -ExclusionPath $env:LOCALAPPDATA"\AMD\DxcCache"
+Add-MpPreference -ExclusionPath $env:LOCALAPPDATA"\AMD\OglCache"
+Add-MpPreference -ExclusionPath $env:windir"\SoftwareDistribution\Datastore\Datastore.edb"
+Add-MpPreference -ExclusionPath $env:windir"\SoftwareDistribution\Datastore\Logs\Edb*.jrs"
+Add-MpPreference -ExclusionPath $env:windir"\SoftwareDistribution\Datastore\Logs\Edb.chk"
+Add-MpPreference -ExclusionPath $env:windir"\SoftwareDistribution\Datastore\Logs\Tmp.edb"
+Add-MpPreference -ExclusionPath $env:windir"\SoftwareDistribution\Datastore\Logs\*.log"
+Add-MpPreference -ExclusionPath $env:windir"\Security\Database\*.edb"
+Add-MpPreference -ExclusionPath $env:windir"\Security\Database\*.sdb"
+Add-MpPreference -ExclusionPath $env:windir"\Security\Database\*.log"
+Add-MpPreference -ExclusionPath $env:windir"\Security\Database\*.chk"
+Add-MpPreference -ExclusionPath $env:windir"\Security\Database\*.jrs"
+Add-MpPreference -ExclusionPath $env:windir"\Security\Database\*.xml"
+Add-MpPreference -ExclusionPath $env:windir"\Security\Database\*.csv"
+Add-MpPreference -ExclusionPath $env:windir"\Security\Database\*.cmtx"
+Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\GroupPolicy\Machine\Registry.pol"
+Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\GroupPolicy\Machine\Registry.tmp"
+Add-MpPreference -ExclusionPath $env:userprofile"\NTUser.dat"
+Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\sru\*.log"
+Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\sru\*.dat"
+Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\sru\*.chk"
+Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\Configuration\MetaConfig.mof"
+Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\winevt\Logs\*.evtx"
+Add-MpPreference -ExclusionPath $env:windir"\apppatch\sysmain.sdb"
+Add-MpPreference -ExclusionPath $env:windir"\EventLog\Data\lastalive?.dat"
+Add-MpPreference -ExclusionProcess ${env:ProgramFiles(x86)}"\Windows Kits\10\Windows Performance Toolkit\WPRUI.exe"
+Add-MpPreference -ExclusionProcess ${env:ProgramFiles(x86)}"\Windows Kits\10\Windows Performance Toolkit\wpa.exe"
+Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\WindowsPowerShell\v1.0\Modules"
+Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\Configuration\DSCStatusHistory.mof"
+Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\Configuration\DSCEngineCache.mof"
+Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\Configuration\DSCResourceStateCache.mof"
+Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\Configuration\ConfigurationStatus"
+Add-MpPreference -ExclusionProcess ${env:ProgramFiles(x86)}"\Common Files\Steam\SteamService.exe"
+
 write-host "setting services" -ForegroundColor red
 sc config AJRouter start= disabled
 sc config DiagTrack start= disabled
