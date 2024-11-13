6d5320b86c9556f982262383f49bd95d2b0a90dc
snipershane0305 <snipershane0305@gmail.com>
2024-11-13 15:19:17 -0500

    changes to descriptions and some settings i think

--- PowershellScript.ps1
+++ PowershellScript.ps1
@@ -3,15 +3,15 @@ pause
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process pwsh.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 
 write-host "merging registry file" -ForegroundColor red
-reg import .\registry.reg
+reg import .\registry.reg #merges the registry.reg registry file!
 
-write-host "Disabling powershell telemetry" -ForegroundColor red #disables powershell 7 telemetry (sends data without benefit)
-[Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT', '1', 'Machine')
+write-host "Disabling powershell telemetry" -ForegroundColor red
+[Environment]::SetEnvironmentVariable('POWERSHELL_TELEMETRY_OPTOUT', '1', 'Machine') #disables powershell 7 telemetry (sends data without benefit)
 
-write-host "removing home and gallery from explorer" -ForegroundColor red #removes buttons from explorer i dont use
-REG DELETE \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace\\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c}\" /f
-REG DELETE \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace\\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}\" /f
-REG ADD \"HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced\" /f /v \"LaunchTo\" /t REG_DWORD /d \"1\"
+write-host "removing home and gallery from explorer" -ForegroundColor red
+REG DELETE \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace\\{e88865ea-0e1c-4e20-9aa6-edcd0212c87c}\" /f #removes buttons from explorer i dont use
+REG DELETE \"HKEY_LOCAL_MACHINE\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Desktop\\NameSpace\\{f874310e-b6b7-47dc-bc84-b9e6b38f5903}\" /f #removes buttons from explorer i dont use 
+REG ADD \"HKEY_CURRENT_USER\\SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Explorer\\Advanced\" /f /v \"LaunchTo\" /t REG_DWORD /d \"1\" #removes buttons from explorer i dont use
 
 write-host "disabling hibernation" -ForegroundColor red
 powercfg.exe /hibernate off #disables hiberation (writes memory to disk and saves power at cost of performance, only useful for laptops)
@@ -20,66 +20,66 @@ write-host "enabling memory compression" -ForegroundColor red
 Enable-MMAgent -mc #enabled memory compression (saves some memory but takes cpu cycles to compress and uncompress the memory)
 
 write-host "applying fsutil settings" -ForegroundColor red
-fsutil behavior set disablecompression 1  #disables ntfs compression
-fsutil behavior set encryptpagingfile 0   #disables encryption on the pagefile.sys file which is disk space that is used as memory and is less performant
-fsutil behavior set mftzone 4             #sets the mft zone to 800MB (the mft zone stores entries about everything about every file)
-fsutil behavior set quotanotify 7200      #sets quota report to 2 hours
+fsutil behavior set disablecompression 1 #disables ntfs compression
+fsutil behavior set encryptpagingfile 0 #disables encryption on the pagefile.sys file which is disk space that is used as memory and is less performant
+fsutil behavior set mftzone 4 #sets the mft zone to 800MB (the mft zone stores entries about everything about every file)
+fsutil behavior set quotanotify 7200 #sets quota report to 2 hours
 fsutil behavior set disabledeletenotify 0 #enables trim on disk
-fsutil behavior set disableLastAccess 1   #disables last access time stamp on directories
-fsutil behavior set disable8dot3 1        #unused file type
+fsutil behavior set disableLastAccess 1 #disables last access time stamp on directories
+fsutil behavior set disable8dot3 1 #unused file type
 
 write-host "applying bcdedits" -ForegroundColor red
-bcdedit /set useplatformtick yes    #uses a hardware timer for ticks which is most reliable
+bcdedit /set useplatformtick yes #uses a hardware timer for ticks which is most reliable
 bcdedit /set disabledynamictick yes #disables platform tick from being dynamic which is more stable
-bcdedit /set useplatformclock no    #disable use of platform clock which is less stable
+bcdedit /set useplatformclock no #disable use of platform clock which is less stable
 bcdedit /set tscsyncpolicy enhanced #sets time stamp counter synchronization policy to enhanced
-bcdedit /set MSI Default            #sets the use of interrupt type to message signaled interrupts which was added for PCI 2.2 which is newer than the old line based interrupts
-bcdedit /set x2apicpolicy Enable    #uses the newer apic mode
+bcdedit /set MSI Default #sets the use of interrupt type to message signaled interrupts which was added for PCI 2.2 which is newer than the old line based interrupts
+bcdedit /set x2apicpolicy Enable #uses the newer apic mode
 bcdedit /deletevalue uselegacyapicmode | Out-Null #deletes old legacy apic mode
-bcdedit /set usephysicaldestination no            #disables physical apic for x2apicpolicy
-bcdedit /set usefirmwarepcisettings no            #disables BIOS PCI resources
-bcdedit /set linearaddress57 OptOut
-bcdedit /set nx OptIn                             #enables data execution prevention which improves security
+bcdedit /set usephysicaldestination no #disables physical apic for x2apicpolicy
+bcdedit /set usefirmwarepcisettings no #disables BIOS PCI resources
+bcdedit /set linearaddress57 OptOut #disables 57 bit virtual memory and keeps it at 48 bit (you dont need 128 petabytes of virtual memory!)
+bcdedit /set nx OptIn #enables data execution prevention which improves security
 
 write-host "applying network settings" -ForegroundColor red
-netsh int tcp set global rss = enabled         #enables recieve side scaling which lets more than one cpu core handle tcp
-netsh int tcp set global prr= enable           #helps a tcp connection from recovering from packet loss quicker for less latency
-netsh int tcp set global initialRto=2000       #lowers initial retransmition timout which helps latency
-Set-NetTCPSetting -SettingName internetcustom -minrto 300
-Set-NetTCPSetting -SettingName internet -minrto 300
-Set-NetTCPSetting -SettingName internetcustom -ScalingHeuristics disabled
-Set-NetTCPSetting -SettingName internet -ScalingHeuristics disabled
-Set-NetTCPSetting -SettingName internetcustom -MaxSynRetransmissions 2
-Set-NetTCPSetting -SettingName internet -MaxSynRetransmissions 2
+netsh int tcp set global rss = enabled #enables recieve side scaling which lets more than one core handle tcp
+netsh int tcp set global prr= enable #helps a tcp connection from recovering from packet loss quicker for less latency
+netsh int tcp set global initialRto=2000 #lowers initial retransmition timout which helps latency
+Set-NetTCPSetting -SettingName internetcustom -minrto 300 #lowers initial retransmition timout which helps latency
+Set-NetTCPSetting -SettingName internet -minrto 300 #lowers initial retransmition timout which helps latency
+Set-NetTCPSetting -SettingName internetcustom -ScalingHeuristics disabled #unneeded feature
+Set-NetTCPSetting -SettingName internet -ScalingHeuristics disabled #unneeded feature
+Set-NetTCPSetting -SettingName internetcustom -MaxSynRetransmissions 2 #sets max retransmitions to 2!
+Set-NetTCPSetting -SettingName internet -MaxSynRetransmissions 2 #sets max retransmitions to 2!
 netsh int tcp set global ecncapability= enable #ecncapability will notify if there is congestion to help packet loss, will only be used if both the client and server support it
-netsh int tcp set global rsc= disable          #disables receive segment coalescing which makes small packets combine, this helps with computing many packets but at the cost of latency
-netsh int tcp set supplemental Template=Internet CongestionProvider=bbr2         #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
-netsh int tcp set supplemental Template=Datacenter CongestionProvider=bbr2       #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
-netsh int tcp set supplemental Template=Compat CongestionProvider=bbr2           #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
+netsh int tcp set global rsc= disable #disables receive segment coalescing which makes small packets combine, this helps with computing many packets but at the cost of latency
+netsh int tcp set supplemental Template=Internet CongestionProvider=bbr2 #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
+netsh int tcp set supplemental Template=Datacenter CongestionProvider=bbr2 #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
+netsh int tcp set supplemental Template=Compat CongestionProvider=bbr2 #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
 netsh int tcp set supplemental Template=DatacenterCustom CongestionProvider=bbr2 #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
-netsh int tcp set supplemental Template=InternetCustom CongestionProvider=bbr2   #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
-netsh int ipv4 set dynamicport tcp start=1025 num=64511
-netsh int ipv4 set dynamicport udp start=1025 num=64511
-netsh int teredo set state disabled #disables teredo
-Set-NetOffloadGlobalSetting -PacketCoalescingFilter Disabled   #disables more coalescing
+netsh int tcp set supplemental Template=InternetCustom CongestionProvider=bbr2 #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
+netsh int ipv4 set dynamicport tcp start=1025 num=65534 #sets the ports tcp can use
+netsh int ipv4 set dynamicport udp start=1025 num=65534 #sets the ports tcp can use
+netsh int teredo set state disabled #disables teredo (used for ipv6)
+Set-NetOffloadGlobalSetting -PacketCoalescingFilter Disabled  #disables more coalescing
 Set-NetOffloadGlobalSetting -ReceiveSegmentCoalescing Disabled #disables more coalescing
 Set-NetOffloadGlobalSetting -Chimney Disabled #forces cpu to handle network instead of NIC
-Enable-NetAdapterChecksumOffload -Name *      #forces cpu to handle network instead of NIC
-Set-NetTCPSetting -SettingName InternetCustom -InitialCongestionWindow 10
+Enable-NetAdapterChecksumOffload -Name * #forces cpu to handle network instead of NIC
+Set-NetTCPSetting -SettingName InternetCustom -InitialCongestionWindow 10 #raises the initial congestion window which makes a tcp connection start with more bandwidth
 Set-NetTCPSetting -SettingName Internet -InitialCongestionWindow 10 #raises the initial congestion window which makes a tcp connection start with more bandwidth
 Disable-NetAdapterLso -Name * #disables large send offload which uses NIC instead of cpu (using the cpu for handing network tasks can help latency if your cpu is strong enough)
 
 write-host "setting dns" -ForegroundColor red
-Set-DnsClientServerAddress -interfaceindex 1 -serveraddresses ("9.9.9.11","9.9.9.9")
-Set-DnsClientServerAddress -interfaceindex 2 -serveraddresses ("9.9.9.11","9.9.9.9")
-Set-DnsClientServerAddress -interfaceindex 3 -serveraddresses ("9.9.9.11","9.9.9.9")
-Set-DnsClientServerAddress -interfaceindex 4 -serveraddresses ("9.9.9.11","9.9.9.9")
-Set-DnsClientServerAddress -interfaceindex 5 -serveraddresses ("9.9.9.11","9.9.9.9")
-Set-DnsClientServerAddress -interfaceindex 6 -serveraddresses ("9.9.9.11","9.9.9.9")
-Set-DnsClientServerAddress -interfaceindex 7 -serveraddresses ("9.9.9.11","9.9.9.9")
-Set-DnsClientServerAddress -interfaceindex 8 -serveraddresses ("9.9.9.11","9.9.9.9")
-Set-DnsClientServerAddress -interfaceindex 9 -serveraddresses ("9.9.9.11","9.9.9.9")
-Set-DnsClientServerAddress -interfaceindex 10 -serveraddresses ("9.9.9.11","9.9.9.9")
+Set-DnsClientServerAddress -interfaceindex 1 -serveraddresses ("9.9.9.11","9.9.9.9") #uses quad9 dns
+Set-DnsClientServerAddress -interfaceindex 2 -serveraddresses ("9.9.9.11","9.9.9.9") #uses quad9 dns
+Set-DnsClientServerAddress -interfaceindex 3 -serveraddresses ("9.9.9.11","9.9.9.9") #uses quad9 dns
+Set-DnsClientServerAddress -interfaceindex 4 -serveraddresses ("9.9.9.11","9.9.9.9") #uses quad9 dns
+Set-DnsClientServerAddress -interfaceindex 5 -serveraddresses ("9.9.9.11","9.9.9.9") #uses quad9 dns
+Set-DnsClientServerAddress -interfaceindex 6 -serveraddresses ("9.9.9.11","9.9.9.9") #uses quad9 dns
+Set-DnsClientServerAddress -interfaceindex 7 -serveraddresses ("9.9.9.11","9.9.9.9") #uses quad9 dns
+Set-DnsClientServerAddress -interfaceindex 8 -serveraddresses ("9.9.9.11","9.9.9.9") #uses quad9 dns
+Set-DnsClientServerAddress -interfaceindex 9 -serveraddresses ("9.9.9.11","9.9.9.9") #uses quad9 dns
+Set-DnsClientServerAddress -interfaceindex 10 -serveraddresses ("9.9.9.11","9.9.9.9") #uses quad9 dns
 
 write-host "setting defender settings" -ForegroundColor red
 set-mppreference -CloudBlockLevel 1
@@ -135,8 +135,6 @@ Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\Configuration\MetaConf
 Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\winevt\Logs\*.evtx"
 Add-MpPreference -ExclusionPath $env:windir"\apppatch\sysmain.sdb"
 Add-MpPreference -ExclusionPath $env:windir"\EventLog\Data\lastalive?.dat"
-Add-MpPreference -ExclusionProcess ${env:ProgramFiles(x86)}"\Windows Kits\10\Windows Performance Toolkit\WPRUI.exe"
-Add-MpPreference -ExclusionProcess ${env:ProgramFiles(x86)}"\Windows Kits\10\Windows Performance Toolkit\wpa.exe"
 Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\WindowsPowerShell\v1.0\Modules"
 Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\Configuration\DSCStatusHistory.mof"
 Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\Configuration\DSCEngineCache.mof"
@@ -144,7 +142,7 @@ Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\Configuration\DSCResou
 Add-MpPreference -ExclusionPath $env:SystemRoot"\System32\Configuration\ConfigurationStatus"
 Add-MpPreference -ExclusionProcess ${env:ProgramFiles(x86)}"\Common Files\Steam\SteamService.exe"
 
-write-host "setting services" -ForegroundColor red
+write-host "setting services" -ForegroundColor red #all these sould be safe!
 sc config AJRouter start= disabled
 sc config DiagTrack start= disabled
 sc config Dhcp start= disabled
