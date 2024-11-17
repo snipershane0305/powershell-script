1c1da5c1812cc661a6c6a42e09ba6a1e7a71a4ae
snipershane0305 <snipershane0305@gmail.com>
2024-11-16 19:14:45 -0500

    changes to network settings

--- PowershellScript.ps1
+++ PowershellScript.ps1
@@ -44,29 +44,41 @@ bcdedit /set nx OptIn #enables data execution prevention which improves security
 write-host "applying network settings" -ForegroundColor red
 netsh int tcp set global rss = enabled #enables recieve side scaling which lets more than one core handle tcp
 netsh int tcp set global prr= enable #helps a tcp connection from recovering from packet loss quicker for less latency
-netsh int tcp set global initialRto=2000 #lowers initial retransmition timout which helps latency
-Set-NetTCPSetting -SettingName internetcustom -minrto 300 #lowers initial retransmition timout which helps latency
-Set-NetTCPSetting -SettingName internet -minrto 300 #lowers initial retransmition timout which helps latency
-Set-NetTCPSetting -SettingName internetcustom -ScalingHeuristics disabled #unneeded feature
-Set-NetTCPSetting -SettingName internet -ScalingHeuristics disabled #unneeded feature
-Set-NetTCPSetting -SettingName internetcustom -MaxSynRetransmissions 2 #sets max retransmitions to 2!
-Set-NetTCPSetting -SettingName internet -MaxSynRetransmissions 2 #sets max retransmitions to 2!
+netsh int tcp set global nonsackrttresiliency=enabled #improves the reliability of tcp over high-latency networks
 netsh int tcp set global ecncapability= enable #ecncapability will notify if there is congestion to help packet loss, will only be used if both the client and server support it
 netsh int tcp set global rsc= disable #disables receive segment coalescing which makes small packets combine, this helps with computing many packets but at the cost of latency
-netsh int tcp set supplemental Template=Internet CongestionProvider=bbr2 #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
-netsh int tcp set supplemental Template=Datacenter CongestionProvider=bbr2 #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
-netsh int tcp set supplemental Template=Compat CongestionProvider=bbr2 #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
-netsh int tcp set supplemental Template=DatacenterCustom CongestionProvider=bbr2 #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
-netsh int tcp set supplemental Template=InternetCustom CongestionProvider=bbr2 #sets tcp congestion provider to bbr2 which is much newer and causes less packet loss
+netsh int teredo set state disabled #disables teredo (used for ipv6)
 netsh int ipv4 set dynamicport tcp start=1025 num=64511 #sets the ports tcp can use
 netsh int ipv4 set dynamicport udp start=1025 num=64511 #sets the ports tcp can use
-netsh int teredo set state disabled #disables teredo (used for ipv6)
+netsh int tcp set supplemental template=internet enablecwndrestart= enabled #enables cwndreset which help the congestion window to change faster allowing for more through put quicker
+netsh int tcp set supplemental template=custom enablecwndrestart= enabled #enables cwndreset which help the congestion window to change faster allowing for more through put quicker
+netsh int tcp set supplemental template=compat enablecwndrestart= enabled #enables cwndreset which help the congestion window to change faster allowing for more through put quicker
+netsh int tcp set supplemental template=datacenter enablecwndrestart= enabled #enables cwndreset which help the congestion window to change faster allowing for more through put quicker
+netsh int tcp set supplemental Template=Internet CongestionProvider=ctcp #sets tcp congestion provider to ctcp which is better for latency and stability
+netsh int tcp set supplemental Template=custom CongestionProvider=ctcp #sets tcp congestion provider to ctcp which is better for latency and stability
+netsh int tcp set supplemental Template=compat CongestionProvider=ctcp #sets tcp congestion provider to ctcp which is better for latency and stability
+netsh int tcp set supplemental Template=datacenter CongestionProvider=ctcp #sets tcp congestion provider to ctcp which is better for latency and stability
+Set-NetTCPSetting -SettingName internet -minrto 300 #lowers initial retransmition timout which helps latency
+Set-NetTCPSetting -SettingName Internetcustom -minrto 300 #lowers initial retransmition timout which helps latency
+Set-NetTCPSetting -SettingName compat -minrto 300 #lowers initial retransmition timout which helps latency
+Set-NetTCPSetting -SettingName internet -ScalingHeuristics disabled #unneeded feature
+Set-NetTCPSetting -SettingName internetcustom -ScalingHeuristics disabled #unneeded feature
+Set-NetTCPSetting -SettingName datacentercustom -ScalingHeuristics disabled #unneeded feature
+Set-NetTCPSetting -SettingName compat -ScalingHeuristics disabled #unneeded feature
+Set-NetTCPSetting -SettingName datacenter -ScalingHeuristics disabled #unneeded feature
+Set-NetTCPSetting -SettingName internet -MaxSynRetransmissions 2 #sets max retransmitions to 2!
+Set-NetTCPSetting -SettingName internetcustom -MaxSynRetransmissions 2 #sets max retransmitions to 2!
+Set-NetTCPSetting -SettingName datacentercustom -MaxSynRetransmissions 2 #sets max retransmitions to 2!
+Set-NetTCPSetting -SettingName compat -MaxSynRetransmissions 2 #sets max retransmitions to 2!
+Set-NetTCPSetting -SettingName datacenter -MaxSynRetransmissions 2 #sets max retransmitions to 2!
+Set-NetTCPSetting -SettingName Internet -InitialCongestionWindow 10 #raises the initial congestion window which makes a tcp connection start with more bandwidth
+Set-NetTCPSetting -SettingName Internetcustom -InitialCongestionWindow 10 #raises the initial congestion window which makes a tcp connection start with more bandwidth
+Set-NetTCPSetting -SettingName datacenter -InitialCongestionWindow 10 #raises the initial congestion window which makes a tcp connection start with more bandwidth
+Set-NetTCPSetting -SettingName datacenter -InitialCongestionWindow 10 #raises the initial congestion window which makes a tcp connection start with more bandwidth
 Set-NetOffloadGlobalSetting -PacketCoalescingFilter Disabled  #disables more coalescing
 Set-NetOffloadGlobalSetting -ReceiveSegmentCoalescing Disabled #disables more coalescing
 Set-NetOffloadGlobalSetting -Chimney Disabled #forces cpu to handle network instead of NIC
 Enable-NetAdapterChecksumOffload -Name * #forces cpu to handle network instead of NIC
-Set-NetTCPSetting -SettingName InternetCustom -InitialCongestionWindow 10 #raises the initial congestion window which makes a tcp connection start with more bandwidth
-Set-NetTCPSetting -SettingName Internet -InitialCongestionWindow 10 #raises the initial congestion window which makes a tcp connection start with more bandwidth
 Disable-NetAdapterLso -Name * #disables large send offload which uses NIC instead of cpu (using the cpu for handing network tasks can help latency if your cpu is strong enough)
 
 write-host "setting dns" -ForegroundColor red
