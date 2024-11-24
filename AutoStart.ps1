43f4777685f289ac9f79727d669a332c019d7196
snipershane0305 <snipershane0305@gmail.com>
2024-11-24 04:52:38 -0500

    removed windows update since its in startup.ps1

--- AutoStart.ps1
+++ AutoStart.ps1
@@ -1,4 +1,3 @@
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process pwsh.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 start-process pwsh.exe -WindowStyle Minimized "C:\StartUp.ps1"
 start-process pwsh.exe -WindowStyle Minimized "C:\AppUpdate.ps1"
-start-process pwsh.exe -WindowStyle Minimized "C:\WindowsUpdate.ps1"
