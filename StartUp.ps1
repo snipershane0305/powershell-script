95ff6c784e0cb7feede1ec08bd506ad66f4f5c2a
snipershane0305 <snipershane0305@gmail.com>
2024-11-24 04:54:28 -0500

    change to update service

--- StartUp.ps1
+++ StartUp.ps1
@@ -12,9 +12,11 @@ write-host "updating system" -ForegroundColor red
 #updates microsoft defender
 C:\"Program Files"\"Windows Defender"\MpCmdRun -SignatureUpdate
 Update-MpSignature -UpdateSource MicrosoftUpdateServer
-#starts needed services for windows update
+#starts needed windows update services
 sc config wuauserv start= demand
 sc config UsoSvc start= demand
+sc config bits start=demand
+net start bits
 net start wuauserv  
 net start usosvc
 start-sleep -seconds 2
