d9083d9b87ca07cb41f1191943815e9dbc42b9ec
snipershane0305 <snipershane0305@gmail.com>
2024-11-24 04:48:12 -0500

    changes to windows update

--- WindowsUpdate.ps1
+++ WindowsUpdate.ps1
@@ -1,16 +1,14 @@
 write-host "updating system" -ForegroundColor red
-C:\"Program Files"\"Windows Defender"\MpCmdRun -SignatureUpdate #updates microsoft defender security
+#updates microsoft defender
+C:\"Program Files"\"Windows Defender"\MpCmdRun -SignatureUpdate
 Update-MpSignature -UpdateSource MicrosoftUpdateServer
-#runs windows update
+#starts needed windows update services
 sc config wuauserv start= demand
 sc config UsoSvc start= demand
 net start wuauserv  
 net start usosvc
 start-sleep -seconds 2
+#runs windows update
 Install-Module PSWindowsUpdate -Confirm:$false
 Add-WUServiceManager -MicrosoftUpdate -Confirm:$false
 Install-WindowsUpdate -MicrosoftUpdate -AcceptAll
-#runs windows update
-write-host "running defender scan" -ForegroundColor red
-C:\"Program Files"\"Windows Defender"\MpCmdRun -scan -ScanType 1
-pause
