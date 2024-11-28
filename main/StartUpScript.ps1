14e858dea4754f4c5c4bd71f29316f9e9862a1e8
snipershane0305 <snipershane0305@gmail.com>
2024-11-28 11:16:14 -0500

    name change

--- main/StartUpScript.ps1
+++ main/StartUpScript.ps1
@@ -1,4 +1,4 @@
 #put this in your startup folder
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process pwsh.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
 start-process pwsh.exe -WindowStyle Minimized "C:\StartUp.ps1"
-start-process pwsh.exe -WindowStyle Minimized "C:\AppUpdate.ps1"
+start-process pwsh.exe -WindowStyle Minimized "C:\PowershellScript.ps1"
