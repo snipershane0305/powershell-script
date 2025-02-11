5559cf80e4ff6f8aa71c1a9eb703e1bab53796a9
snipershane0305 <snipershane0305@gmail.com>
2025-02-11 02:13:48 -0500

    Update StartUpScript.ps1

--- main/StartUpScript.ps1
+++ main/StartUpScript.ps1
@@ -1,3 +1,5 @@
 #put this in your startup folder
 if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process pwsh.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
-start-process pwsh.exe -WindowStyle Minimized "C:\PowershellScript.ps1"
+$systemDrive = (Get-WmiObject -Class Win32_OperatingSystem).SystemDrive
+start-process pwsh.exe -WindowStyle Minimized "$systemDrive\PowershellScript.ps1"
+start-process pwsh.exe -WindowStyle Minimized "$systemDrive\SystemUpdate.ps1"
