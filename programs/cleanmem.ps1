a755bb702840f5bf1c2671f2fc4368cb2c1608a6
snipershane0305 <snipershane0305@gmail.com>
2025-02-11 01:34:43 -0500

    Add files via upload

---
+++ programs/cleanmem.ps1
@@ -0,0 +1,2 @@
+if (!([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { Start-Process pwsh.exe "-NoProfile -ExecutionPolicy Bypass -File `"$PSCommandPath`"" -Verb RunAs; exit }
+C:\memreduct.exe -clean:full
